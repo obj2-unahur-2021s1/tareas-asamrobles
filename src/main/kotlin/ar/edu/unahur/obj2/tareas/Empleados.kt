49e8d5685ac8664bc672d6e8f42abcef030a1e0e
Luciano Robles <soylucho@hotmail.es>
2021-06-05 20:53:10 -0300

    Todavia No Anda nada porque no hay test. Primer commit cambio de manos

---
+++ src/main/kotlin/ar/edu/unahur/obj2/tareas/Empleados.kt
@@ -0,0 +1,5 @@
+package ar.edu.unahur.obj2.tareas
+
+open class Empleados(val cuantoCobraPorHora :Int) {
+    fun cuantoCobraUnEmpleadoPorHora() = cuantoCobraPorHora
+}
--- src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
@@ -1,3 +1,23 @@
 package ar.edu.unahur.obj2.tareas
 
-class Tarea
+class Tarea (val horasEstimadas : Int,val costoDeInfraestructura : Int,val responsable: Responsable) {
+    fun horasNecesariasParaFinalizar() : Int {
+        this.horasEstimadas / responsable.cantDeEmpleados()
+    }
+    fun costoResponsable () = responsable.cuantoCobraPorHora * horasEstimadas
+    //fun costoTotalDeEmpleados () =
+    fun costoPorEmpleado () = responsable.empleados.map { it.cuantoCobraPorHora }.forEach{a -> a * this.horasNecesariasParaFinalizar()}
+    fun costoTotalInfraestructura () = costoResponsable() + costoDeInfraestructura
+
+
+}
+
+
+
+class Responsable (val empleados: MutableList<Empleados>,val cuantoCobraPorHora :Int){
+
+    fun cantDeEmpleados() = empleados.size
+
+
+}
+

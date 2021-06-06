d75af557c8ef04e01a6b39497a9a1e1ea4db10ea
ferAsam <ferasam88@gmail.com>
2021-06-05 21:33:41 -0300

    testeado costos en tarea y cambiado a double

--- src/test/kotlin/ar/edu/unahur/obj2/tareas/TareaTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/tareas/TareaTest.kt
@@ -5,15 +5,15 @@ import io.kotest.matchers.shouldBe
 
 class TareaTest : DescribeSpec({
   describe("Una tarea") {
-    val empleado20p = Empleados(20)
-    val empleado30p = Empleados(30)
-    val empleado40p = Empleados(40)
-    val empleado50p = Empleados(50)
-    val empleado60p = Empleados(60)
+    val empleado20p = Empleados(20.0)
+    val empleado30p = Empleados(30.0)
+    val empleado40p = Empleados(40.0)
+    val empleado50p = Empleados(50.0)
+    val empleado60p = Empleados(60.0)
 
     val responsable100 = Responsable(mutableListOf(empleado20p, empleado30p, empleado40p, empleado50p, empleado60p), 100)
 
-    val tareaRandom = Tarea(50, 1000, responsable100)
+    val tareaRandom = Tarea(50, 1000.0, responsable100)
 
     describe("probando una tarea") {
 
